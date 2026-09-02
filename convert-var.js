fa1040c620bf9e29569431c939c678b77df08d26
EMERSON RONALD PEREIRA <104523253+EmerRPereira@users.noreply.github.com>
2026-09-02 19:36:26 -0300

    Convert Var

---
+++ convert-var.js
@@ -0,0 +1,9 @@
+// Convert this old code to modern JavaScript:
+function calculate() {
+    var result = 0;
+    var multiplier = 2;
+    for (var i = 0; i < 5; i++) {
+        result += i * multiplier;
+    }
+    console.log(result);
+}
